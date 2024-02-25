c797cc06ca904fa7cc64af8ff209f507c30a7cb7
ERECTUMTOUCHER <75875960+ERECTUMTOUCHER@users.noreply.github.com>
2024-02-24 16:56:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var instances = document.querySelectorAll(".hs__wrapper");
+  var slider = document.querySelector("#announcement-slider .slider");
+  var slides = document.querySelectorAll("#announcement-slider .slide");
+  var prevButton = document.querySelector("#announcement-slider .prev");
+  var nextButton = document.querySelector("#announcement-slider .next");
+  var slideIndex = 0;
 
-  instances.forEach(function(instance) {
-    var arrows = instance.querySelectorAll(".arrow");
-    var prevArrow = instance.querySelector('.arrow-prev');
-    var nextArrow = instance.querySelector('.arrow-next');
-    var box = instance.querySelector(".hs");
-    var x = 0;
-    var mx = 0;
-    var maxScrollWidth = box.scrollWidth - (box.clientWidth / 2) - (box.offsetWidth / 2);
+  showSlide(slideIndex);
 
-    arrows.forEach(function(arrow) {
-      arrow.addEventListener('click', function() {
-        if (!this.classList.contains("disabled")) {
-          if (this.classList.contains("arrow-next")) {
-            x = ((box.offsetWidth / 2)) + box.scrollLeft - 10;
-            box.scrollTo({
-              left: x,
-              behavior: 'smooth'
-            });
-          } else {
-            x = ((box.offsetWidth / 2)) - box.scrollLeft - 10;
-            box.scrollTo({
-              left: -x,
-              behavior: 'smooth'
-            });
-          }
-        }
-      });
+  function showSlide(index) {
+    slides.forEach(function(slide) {
+      slide.style.display = "none";
     });
+    if (index >= slides.length) { slideIndex = 0; }
+    if (index < 0) { slideIndex = slides.length - 1; }
+    slides[slideIndex].style.display = "block";
+  }
 
-    box.addEventListener('mousemove', function(e) {
-      var mx2 = e.pageX - this.offsetLeft;
-      if (mx) this.scrollLeft = this.sx + mx - mx2;
-    });
+  function moveSlide(n) {
+    showSlide(slideIndex += n);
+  }
 
-    box.addEventListener('mousedown', function(e) {
-      this.sx = this.scrollLeft;
-      mx = e.pageX - this.offsetLeft;
-    });
+  prevButton.addEventListener("click", function() {
+    moveSlide(-1);
+  });
 
-    box.addEventListener('scroll', function() {
-      toggleArrows();
-    });
-
-    document.addEventListener("mouseup", function() {
-      mx = 0;
-    });
-
-    function toggleArrows() {
-      if (box.scrollLeft > maxScrollWidth - 10) {
-        nextArrow.classList.add('disabled');
-      } else if (box.scrollLeft < 10) {
-        prevArrow.classList.add('disabled');
-      } else {
-        nextArrow.classList.remove('disabled');
-        prevArrow.classList.remove('disabled');
-      }
-    }
+  nextButton.addEventListener("click", function() {
+    moveSlide(1);
   });
 });
