b66c5bda590ccec9350d0a3089702ace849aa495
ERECTUMTOUCHER <75875960+ERECTUMTOUCHER@users.noreply.github.com>
2024-02-24 19:18:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     var slider = document.querySelector("#announcement-slider .slider");
     var slides = document.querySelectorAll("#announcement-slider .slide");
     var prevButton = document.querySelector("#announcement-slider .prev");
@@ -10,53 +9,42 @@
     showSlide(slideIndex);
 
     function showSlide(index) {
-      slides.forEach(function(slide) {
-        slide.style.opacity = 0;
-        slide.style.transition = "opacity 0.5s ease";
-      });
-      if (index >= slides.length) { slideIndex = 0; }
-      if (index < 0) { slideIndex = slides.length - 1; }
-      slides[slideIndex].style.opacity = 1;
+        slides.forEach(function(slide) {
+            slide.style.opacity = 0;
+            slide.style.transition = "opacity 0.5s ease";
+        });
+        if (index >= slides.length) { slideIndex = 0; }
+        if (index < 0) { slideIndex = slides.length - 1; }
+        slides[slideIndex].style.opacity = 1;
     }
 
     function moveSlide(n) {
-      showSlide(slideIndex += n);
+        showSlide(slideIndex += n);
     }
 
     prevButton.addEventListener("click", function() {
-      moveSlide(-1);
+        moveSlide(-1);
     });
 
     nextButton.addEventListener("click", function() {
-      moveSlide(1);
+        moveSlide(1);
     });
 
     var slideTimer = setInterval(function() {
-      moveSlide(1);
+        moveSlide(1);
     }, slideInterval);
 
     slider.addEventListener("mouseenter", function() {
-      clearInterval(slideTimer);
+        clearInterval(slideTimer);
     });
 
     slider.addEventListener("mouseleave", function() {
-      slideTimer = setInterval(function() {
-        moveSlide(1);
-      }, slideInterval);
+        slideTimer = setInterval(function() {
+            moveSlide(1);
+        }, slideInterval);
     });
-  });
-// add classes for mobile navigation toggling
-    var CSbody = document.querySelector("body");
-    const CSnavbarMenu = document.querySelector("#cs-navigation");
-    const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
+});
 
-    CShamburgerMenu.addEventListener('click', function() {
-        CShamburgerMenu.classList.toggle("cs-active");
-        CSnavbarMenu.classList.toggle("cs-active");
-        CSbody.classList.toggle("cs-open");
-        // run the function to check the aria-expanded value
-        ariaExpanded();
-    });
 
     // checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
     function ariaExpanded() {
