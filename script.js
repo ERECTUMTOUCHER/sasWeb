298994d7d8fc4681dc5ef43ce47fed01e28a4f7f
ERECTUMTOUCHER <75875960+ERECTUMTOUCHER@users.noreply.github.com>
2024-03-07 00:12:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -45,41 +45,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-
-    // checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
-    function ariaExpanded() {
-        const csUL = document.querySelector('#cs-expanded');
-        const csExpanded = csUL.getAttribute('aria-expanded');
-
-        if (csExpanded === 'false') {
-            csUL.setAttribute('aria-expanded', 'true');
-        } else {
-            csUL.setAttribute('aria-expanded', 'false');
-        }
-    }
-
-        // This script adds a class to the body after scrolling 100px
-    // and we used these body.scroll styles to create some on scroll 
-    // animations with the navbar
-    
-    document.addEventListener('scroll', (e) => { 
-        const scroll = document.documentElement.scrollTop;
-        if(scroll >= 100){
-    document.querySelector('body').classList.add('scroll')
-        } else {
-        document.querySelector('body').classList.remove('scroll')
-        }
-    });
-
-
-    // mobile nav toggle code
-    const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
-        for (const item of dropDowns) {
-            const onClick = () => {
-            item.classList.toggle('cs-active')
-        }
-        item.addEventListener('click', onClick)
-        }
 // add classes for mobile navigation toggling
     var CSbody = document.querySelector("body");
     const CSnavbarMenu = document.querySelector("#cs-navigation");
@@ -126,5 +91,6 @@ document.addEventListener("DOMContentLoaded", function() {
             item.classList.toggle('cs-active')
         }
         item.addEventListener('click', onClick)
-        }   
+        }
+                                
 </script>
